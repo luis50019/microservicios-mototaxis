1d2f25391ea64af904013b126c483e889740cc87
AsaelLopez21 <asaellopezinfo@gmainl.com>
2025-11-06 20:37:37 -0600

    fix/add new error listeners websocket

---
+++ MicroServicio.WebSocketReservacion/src/ServiceReservation.Application/DTOs/ResponseErrorRideFare.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceReservation.Application.DTOs
+{
+    public class ResponseErrorRideFare
+    {
+        string MessageError { get; set; } = string.Empty;
+        string DetailError { get; set; } = string.Empty;
+        string Suggest { get; set; } = string.Empty;
+        string IdClient { get; set; } = string.Empty;
+        int CodeStatus { get; set; } 
+    }
+}
